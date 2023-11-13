6fa9f233213cfd43309d55e572ab11032f89a938
Ayan Khan <impracticalayan@gmail.com>
2023-11-14 00:07:34 +0530

    First Lecture of Node

---
+++ Tutorial-05/index.js
@@ -0,0 +1,18 @@
+const { error } = require('console');
+const parse = require('csv-parse')
+
+const fs= require('fs')
+
+const results = []
+
+fs.createReadStream('keplerdata.csv')
+    .on('data',(data)=>{
+        results.push(data);
+    })
+    .on('error',(err)=>{
+        console.log(err)
+    })
+    .on('end',()=>{
+        console.log(results)
+        console.log('done')
+    })
